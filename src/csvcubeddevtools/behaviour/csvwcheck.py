cf8bfcb22d783dc526fce19cf6ab61d67d0a1af3
NickPapONS <113980059+NickPapONS@users.noreply.github.com>
2023-08-02 15:00:51 +0100

    Removing csvlint and changing windows tests to csvw-check (#10)
    * Bump requests from 2.28.1 to 2.31.0
    
    Bumps [requests](https://github.com/psf/requests) from 2.28.1 to 2.31.0.
    - [Release notes](https://github.com/psf/requests/releases)
    - [Changelog](https://github.com/psf/requests/blob/main/HISTORY.md)
    - [Commits](https://github.com/psf/requests/compare/v2.28.1...v2.31.0)
    
    ---
    updated-dependencies:
    - dependency-name: requests
      dependency-type: indirect
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Bump certifi from 2022.9.14 to 2023.7.22
    
    Bumps [certifi](https://github.com/certifi/python-certifi) from 2022.9.14 to 2023.7.22.
    - [Commits](https://github.com/certifi/python-certifi/compare/2022.09.14...2023.07.22)
    
    ---
    updated-dependencies:
    - dependency-name: certifi
      dependency-type: indirect
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Update and rename csvlint.py to csvwcheck.py
    
    Removing csvlint from behaviour test steps.
    
    * Delete test_csvlint.py
    
    These tests are no longer needed because we are removing csvlint from this repository.
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- src/csvcubeddevtools/behaviour/csvwcheck.py
+++ src/csvcubeddevtools/behaviour/csvwcheck.py
@@ -1,8 +1,8 @@
 """
-csvlint
+csvwcheck
 -------
 
-behave functionality to run csv-lint on some output
+behave functionality to run csvw-check on some output
 """
 from behave import *
 from pathlib import Path
@@ -16,27 +16,8 @@ from csvcubeddevtools.helpers.shell import run_command_in_dir
 from .dockerornot import SHOULD_USE_DOCKER
 
 if SHOULD_USE_DOCKER:
-    client = docker.from_env()
-    client.images.pull("gsscogs/csvlint")
     csvwcheckclient = docker.from_env()
     csvwcheckclient.images.pull("gsscogs/csvw-check:latest")
-
-
-def _run_csvlint(metadata_file_path: Path) -> Tuple[int, str]:
-    if SHOULD_USE_DOCKER:
-        csvlint = client.containers.create(
-            "gsscogs/csvlint", command=f"csvlint -s '/tmp/{metadata_file_path.name}'"
-        )
-        csvlint.put_archive("/tmp", dir_to_tar(metadata_file_path.parent))
-        csvlint.start()
-        response: dict = csvlint.wait()
-        exit_code = response["StatusCode"]
-        sys.stdout.write(csvlint.logs().decode("utf-8"))
-        return exit_code, csvlint.logs().decode("utf-8")
-    else:
-        return run_command_in_dir(
-            f'csvlint -s "{metadata_file_path.resolve()}"', metadata_file_path.parent
-        )
 
 def _run_csvwcheck(metadata_file_path: Path) -> Tuple[int, str]:
     if SHOULD_USE_DOCKER:
@@ -50,32 +31,9 @@ def _run_csvwcheck(metadata_file_path: Path) -> Tuple[int, str]:
         sys.stdout.write(csvwcheck.logs().decode("utf-8"))
         return exit_code, csvwcheck.logs().decode("utf-8")
     else:
-        # If SHOULD_USE_DOCKER is false then we assume we are in the windows environment. Windows tests will be done as part of next ticket
         return run_command_in_dir(
-            f'csvlint -s "{metadata_file_path.resolve()}"', metadata_file_path.parent
+            f'bin/csvw-check -s "{metadata_file_path.resolve()}"', metadata_file_path.parent
         )
-
-@step("csvlint validation of all CSV-Ws should succeed")
-def step_impl(context):
-    temp_dir = get_context_temp_dir_path(context)
-    for file in temp_dir.rglob("*.csv-metadata.json"):
-        exit_code, logs = _run_csvlint(temp_dir / file)
-        assert exit_code == 0, logs
-
-
-@step('csvlint validation of "{file}" should succeed')
-def step_impl(context, file: str):
-    temp_dir = get_context_temp_dir_path(context)
-    exit_code, logs = _run_csvlint(temp_dir / file)
-    assert exit_code == 0, logs
-
-
-@step('csvlint validation of "{file}" should fail with "{expected}"')
-def step_impl(context, file: str, expected: str):
-    temp_dir = get_context_temp_dir_path(context)
-    exit_code, logs = _run_csvlint(temp_dir / file)
-    assert exit_code == 1, logs
-    assert expected in logs, logs
 
 
 @step("csvwcheck validation of all CSV-Ws should succeed")
